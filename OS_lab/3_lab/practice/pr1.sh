215896f24c70946216c20c6ef4f7ae3a9861a405
mufeezhanif <mufeez.hanif123@gmail.com>
2025-03-01 11:19:07 +0500

    after mid 1

---
+++ OS_lab/3_lab/practice/pr1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo tunjo naalo chah aahe
+read name
+
+echo dubara wadaien
+read name1
+read -p "dubara wadaeen: " name1
+echo $name
+echo "$name1"
+
---
+++ OS_lab/3_lab/practice/practice.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+<<thisisacomment
+		#	first 
+echo "Hello World!"
+
+var1=something
+lscommand=$(ls)
+
+echo $lscommand
+echo "$var1"
+
+
+
+
+#multiline comment
+
+
+
+adsdhjh
+
+
+thisisacomment
+
+
+
+read num
+if [ $num -eq 10 ]
+then 
+	echo Yes you are ten
+else
+	echo Yes you are not ten
+fi
